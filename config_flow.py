af01e8d84e4b223366d5036116325f05f3f336f5
Sam Banning <samcbanning@gmail.com>
2023-06-30 16:12:32 -0400

    progress

---
+++ config_flow.py
@@ -0,0 +1,8 @@
+from homeassistant import config_entries
+
+class GasMonitorConfigFlow(config_entries.ConfigFlow, domain="gas_monitor"):
+    async def async_step_user(self, user_input=None):
+        # This is the configuration step. 
+        # You can use `user_input` to get the data from the YAML file.
+        # This function should return a dict like this:
+        return self.async_create_entry(title="Gas Monitor", data=user_input)
