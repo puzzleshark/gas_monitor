b809c6dc3306e6e3770145ccc5cdb4e196022cb8
Sam Banning <samcbanning@gmail.com>
2023-06-30 17:42:25 -0400

    progress

--- __init__.py
+++ __init__.py
@@ -1,11 +1,46 @@
+from dataclasses import dataclass
+import logging
+
+from home_assistant_bluetooth import BluetoothServiceInfoBleak
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.components.bluetooth import BluetoothScanningMode
+from homeassistant.components.bluetooth.passive_update_processor import PassiveBluetoothProcessorCoordinator
+from homeassistant.const import Platform
+
+_LOGGER = logging.getLogger(__name__)
+
+
 DOMAIN = "gas_monitor"
 
+def my_parser(service_info: BluetoothServiceInfoBleak):
+    _LOGGER.info(service_info)
+    return service_info.name
 
-def setup(hass, config):
-    hass.states.set("gas_monitor.value", 10)
+
+# def setup(hass, config):
+#     hass.states.set("gas_monitor.value", 10)
+
+#     # Return boolean to indicate that initialization was successful.
+#     return True
+
+def async_setup(hass, config):
+    _LOGGER.info(config)
+    hass.states.async_set("gas_monitor.percentage", 10)
+
+    coordinator = PassiveBluetoothProcessorCoordinator(
+        hass,
+        _LOGGER,
+        address="F4:55:1D:09:DC:34",
+        mode=BluetoothScanningMode.ACTIVE,
+        update_method=my_parser
+    )
+    coordinator.async_start()
 
     # Return boolean to indicate that initialization was successful.
     return True
+    
 
 
 # from dataclasses import dataclass
