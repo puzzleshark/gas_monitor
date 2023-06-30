e58bd0d4c430048859b9f82874b35d9a326e69da
Sam Banning <samcbanning@gmail.com>
2023-06-30 16:42:00 -0400

    maybe better

--- config_flow.py
+++ config_flow.py
@@ -1,8 +1,19 @@
 from homeassistant import config_entries
 
+# class GasMonitorConfigFlow(config_entries.ConfigFlow, domain="gas_monitor"):
+#     async def async_step_user(self, user_input=None):
+#         # This is the configuration step. 
+#         # You can use `user_input` to get the data from the YAML file.
+#         # This function should return a dict like this:
+#         return self.async_create_entry(title="Gas Monitor", data=user_input)
+
+import voluptuous as vol
+
 class GasMonitorConfigFlow(config_entries.ConfigFlow, domain="gas_monitor"):
-    async def async_step_user(self, user_input=None):
-        # This is the configuration step. 
-        # You can use `user_input` to get the data from the YAML file.
-        # This function should return a dict like this:
-        return self.async_create_entry(title="Gas Monitor", data=user_input)
+    async def async_step_user(self, info):
+        if info is not None:
+            pass
+
+        return self.async_show_form(
+            step_id="device", data_schema=vol.Schema({vol.Required("mac"): str})
+        )
