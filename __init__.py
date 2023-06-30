4bb622f87647b4136b601d61ca54279414fc7646
Sam Banning <samcbanning@gmail.com>
2023-06-30 10:47:48 -0400

    maybe

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ DOMAIN = "gas_monitor"
 
 
 def setup(hass, config):
-    hass.states.set("hello_state.world", "Paulus")
+    hass.states.set("gas_monitor.world", "Paulus")
 
     # Return boolean to indicate that initialization was successful.
     return True
